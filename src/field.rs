602a8ba720eaeb6e5c7e1123e81b1c13d5a816ac
bzadorozhnyi <bohzador@gmail.com>
2025-05-19 19:07:08 +0300

    Added Bresenham's algo to improve cells' filling and erasing process

--- src/field.rs
+++ src/field.rs
@@ -12,6 +12,7 @@ pub struct Field {
     pub start_cell: Option<Cell>,
     pub end_cell: Option<Cell>,
     pub path: Option<Vec<Cell>>,
+    pub line_segment_start: Option<Pos2>,
     response: Option<Response>,
     painter: Option<Painter>,
 }
@@ -25,6 +26,7 @@ impl Default for Field {
             start_cell: None,
             end_cell: None,
             path: None,
+            line_segment_start: None,
             response: None,
             painter: None,
         }
@@ -246,27 +248,77 @@ impl Field {
         }
     }
 
+    fn pointer_click_pos(&self) -> Option<Pos2> {
+        self.response().interact_pointer_pos()
+    }
+
     fn clicked_cell(&self) -> Option<Cell> {
-        self.pos2cell(self.response().interact_pointer_pos())
+        self.pos2cell(self.pointer_click_pos())
     }
 
     pub fn hovered_cell(&self) -> Option<Cell> {
         self.pos2cell(self.response().hover_pos())
     }
 
-    pub fn handle_adding_cells(&mut self) {
-        if let Some(cell) = self.clicked_cell() {
-            if self.start_cell.as_ref() == Some(&cell) || self.end_cell.as_ref() == Some(&cell) {
-                return;
-            }
-            self.filled_cells.insert(cell);
+    fn is_endpoint(&self, cell: &Cell) -> bool {
+        self.start_cell == Some(*cell)
+    }
+
+    fn bresenham_cells(&self, mut start_cell: Cell, end_cell: Cell) -> HashSet<Cell> {
+        let dx = (end_cell.x as i32 - start_cell.x as i32).abs();
+        let dy = -(end_cell.y as i32 - start_cell.y as i32).abs();
+
+        let sx: i32 = if start_cell.x < end_cell.x { 1 } else { -1 };
+        let sy: i32 = if start_cell.y < end_cell.y { 1 } else { -1 };
+
+        let mut err = dx + dy;
+
+        let mut cells = HashSet::new();
+        if !self.is_endpoint(&start_cell) {
+            cells.insert(start_cell);
         }
+
+        while start_cell != end_cell {
+            let err2 = 2 * err;
+
+            if err2 >= dy {
+                err += dy;
+                start_cell.x = (start_cell.x as i32 + sx) as usize;
+            }
+
+            if err2 <= dx {
+                err += dx;
+                start_cell.y = (start_cell.y as i32 + sy) as usize;
+            }
+
+            if !self.is_endpoint(&start_cell) {
+                cells.insert(start_cell);
+            }
+        }
+
+        cells
+    }
+
+    pub fn handle_adding_cells(&mut self) {
+        if let (Some(start_cell), Some(end_cell)) = (
+            self.pos2cell(self.line_segment_start),
+            self.pos2cell(self.pointer_click_pos()),
+        ) {
+            let cells_touched_by_line = self.bresenham_cells(start_cell, end_cell);
+            self.filled_cells.extend(cells_touched_by_line);
+        }
+        self.line_segment_start = self.pointer_click_pos();
     }
 
     pub fn handle_removing_cells(&mut self) {
-        if let Some(cell) = self.clicked_cell() {
-            self.filled_cells.remove(&cell);
+        if let (Some(start_cell), Some(end_cell)) = (
+            self.pos2cell(self.line_segment_start),
+            self.pos2cell(self.pointer_click_pos()),
+        ) {
+            let cells_touched_by_line = self.bresenham_cells(start_cell, end_cell);
+            self.filled_cells.retain(|x| !cells_touched_by_line.contains(x));
         }
+        self.line_segment_start = self.pointer_click_pos();
     }
 
     pub fn handle_start_cell_selection(&mut self) {
