30ba794e107a888cbfe0b253395fa755375dbbb7
bzadorozhnyi <bohzador@gmail.com>
2025-07-26 22:15:29 +0300

    moved PinkPairParam to separate file and code refactor;

--- src/template/mod.rs
+++ src/template/mod.rs
@@ -1,14 +1,12 @@
-use std::collections::HashSet;
+mod param_pink;
 
 use tera::Tera;
 
 use crate::{
     error::AppError,
-    field::{
-        cell::{Cell, CellType},
-        Field,
-    },
+    field::{cell::CellType, Field},
     neos::solver::Solver,
+    template::param_pink::PinkPairParam,
 };
 
 #[derive(Clone, Copy, Debug, PartialEq)]
@@ -58,23 +56,19 @@ impl Template {
         if field.start_cell.is_none() {
             return Err(AppError::StartNotSet);
         }
+        context.insert("start_x", &(field.start_cell.as_ref().unwrap().x));
+        context.insert("start_y", &(field.start_cell.as_ref().unwrap().y));
 
         if field.end_cell.is_none() {
             return Err(AppError::EndNotSet);
         }
-
-        context.insert("start_x", &(field.start_cell.as_ref().unwrap().x));
-        context.insert("start_y", &(field.start_cell.as_ref().unwrap().y));
-
         context.insert("end_x", &(field.end_cell.as_ref().unwrap().x));
         context.insert("end_y", &(field.end_cell.as_ref().unwrap().y));
 
-        // pink constraints info
         let pink_pair_range: Vec<usize> = (1..=(field.pink_pair_map.len() / 2)).collect();
-        let pink_pairs = field.unique_pink_pairs();
-
         context.insert("pink_pair_range", &pink_pair_range);
-        context.insert("pink_pairs", &prepare_pink_params(pink_pairs));
+        let pink_pairs = field.unique_pink_pairs();
+        context.insert("pink_pairs", &PinkPairParam::new(pink_pairs));
 
         if let Template::TurnCost(turn_cost) = self {
             context.insert("turn_cost", turn_cost);
@@ -91,11 +85,9 @@ impl Template {
                 .join(" "),
         );
 
-        let code = tera
+        let ampl_code = tera
             .render(&format!("{}.tera", self.name()), &context)
             .map_err(|_| AppError::FailedRenderFile)?;
-
-        println!("{}", code);
 
         let xml_input = format!(
             "
@@ -115,7 +107,7 @@ impl Template {
             ",
             solver.name(),
             email,
-            code
+            ampl_code
         );
 
         Ok(xml_input)
@@ -134,26 +126,4 @@ impl Template {
             Template::Pink => "path_pink",
         }
     }
-}
-
-#[derive(serde::Serialize)]
-struct PinkPairParam {
-    name: String,
-    values: Vec<usize>,
-}
-
-fn prepare_pink_params(pink_pairs: HashSet<(&Cell, &Cell)>) -> Vec<PinkPairParam> {
-    let mut result = Vec::new();
-    let mut counter = 1;
-
-    for (a, b) in pink_pairs {
-        let values = vec![1, a.x, 2, a.y, 3, b.x, 4, b.y];
-        result.push(PinkPairParam {
-            name: format!("pink_pair{}", counter),
-            values,
-        });
-        counter += 1;
-    }
-
-    result
 }
